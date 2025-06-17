c189cd818fda36cebbcb0cc4424e3ddbfe8862a8
MAU143429 <mau141226@estudiantec.cr>
2025-06-16 21:02:55 -0600

    Update vga.sv

--- vga.sv
+++ vga.sv
@@ -1,20 +1,13 @@
 module vga (
-    input logic clk, enter,
+    input logic clk, enter, clk_25,
+	 input logic [7:0] pixel_data,
+	 output logic [18:0] pixel_address,
     output logic vga_hsync, vga_vsync, sync_blank, sync_b,
-    output logic [7:0] red, green, blue,
-    output logic clk_25
+    output logic [7:0] red, green, blue
 );
     // Señales internas
     logic [9:0] hs, vs;
-    logic [18:0] pixel_address;  // 19 bits para direccionar 460x460x2 imágenes
-    logic [7:0] pixel_data;
-	 logic [18:0] wr_address = 19'b0;
-    logic [7:0] wr_data = 8'b0;
-    logic wren = 1'b0;
-    
-    // Instancias
-    clkdiv div(.clk(clk), .clk_25(clk_25));
-    
+	 
     gcontroller controller(
         .clk_25(clk_25),
         .vga_hsync(vga_hsync),
@@ -37,17 +30,4 @@ module vga (
         .pixel_address(pixel_address)
     );
     
-    // Instancia de la RAM generada por el IP
-     ram ram_inst (
-        .address_a(pixel_address),
-        .address_b(19'b0),       // No usado
-        .clock(clk_25),
-        .data_a(8'b0),           // No usado
-        .data_b(8'b0),           // No usado
-        .wren_a(1'b0),           // Siempre lectura
-        .wren_b(1'b0),           // Deshabilitado
-        .q_a(pixel_data),       // Salida a VGA
-        .q_b()                  // No usado
-    );
-
 endmodule
